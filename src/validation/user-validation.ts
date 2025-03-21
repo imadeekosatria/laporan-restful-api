305a7dc5c112665303cf1c2488db053c4f284135
imadeekosatria <imadeekosatriawiguna@gmail.com>
2025-03-21 19:50:02 +0800

    feat: make user name column optional in database schema and update validation logic

--- src/validation/user-validation.ts
+++ src/validation/user-validation.ts
@@ -5,7 +5,7 @@ export class UserValidation{
     static readonly REGISTER = z.object({
         username: z.string().min(1).max(100),
         password: z.string().min(1).max(100),
-        name: z.string().min(1).max(100)
+        name: z.string().min(1).max(100).optional()
     })
 
     static readonly LOGIN = z.object({
